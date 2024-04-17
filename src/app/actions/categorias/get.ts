d311b2aa8823dc8313321010602ae96b72e62947
joaocarloslima <joaocarloslima@me.com>
2024-04-16 22:46:19 -0300

    editar categoria

--- src/app/actions/categorias/get.ts
+++ src/app/actions/categorias/get.ts
@@ -1,6 +1,7 @@
 "use server"
 
 export async function getCategorias() {
+    await new Promise(r => setTimeout(r, 10000))
     const resp = await fetch(`${process.env.API_BASE_URL}/categoria`, { next: { revalidate: 0 } })
     return await resp.json()
 }
--- src/app/categorias/[id]/EditForm.tsx
+++ src/app/categorias/[id]/EditForm.tsx
@@ -7,6 +7,7 @@ import { Autocomplete, AutocompleteItem, Button, Input } from "@nextui-org/react
 
 import { useFormState } from "react-dom";
 import { create } from "@/app/actions/categorias/create";
+import { update } from "@/app/actions/categorias/update";
 
 
 export function EditForm(categoria : Categoria) {
@@ -15,16 +16,17 @@ export function EditForm(categoria : Categoria) {
         message: ""
     }
 
-
-    const [state, formAction] = useFormState(create, initialState)
+    const [state, formAction] = useFormState(update, initialState)
     return (
         <form action={formAction} className="flex flex-col gap-4 p-6 m-6 bg-slate-900 rounded min-w-[500px]">
+            <input type="hidden" name="id" value={categoria.id} />
             <h2 className="text-2xl font-bold">Editar Categoria {categoria.nome}</h2>
             <Input
                 key="nome"
                 label="Nome"
                 name="nome"
                 variant="bordered"
+                defaultValue={categoria.nome}
                 labelPlacement="outside"
                 isInvalid={state?.message != ""}
                 errorMessage={state?.message}
@@ -33,6 +35,7 @@ export function EditForm(categoria : Categoria) {
             <Autocomplete
                 label="Ícone"
                 placeholder="buscar ícone"
+                defaultInputValue={categoria.icone}
                 variant="bordered"
                 name="icone"
                 labelPlacement="outside"
