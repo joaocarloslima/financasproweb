c5b77112c2f4092a15504b623e0a821c7ccedb2f
joaocarloslima <joaocarloslima@me.com>
2024-04-22 19:12:56 -0300

    Add Movimentacoes page with components and functionality

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,14 @@
-interface Categoria{
+interface Categoria {
     id: number,
     nome: string,
     icone: string
+}
+
+interface Movimentacao {
+    id: number,
+    descricao: string,
+    valor: number,
+    data: string,
+    tipo: "ENTRADA" | "SAIDA",
+    categoria: Categoria
 }
